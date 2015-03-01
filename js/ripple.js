fd11c99a939cd6bad86dddfce7c30f270e6d60e2
hejiheji001@gmail.com <hejiheji001@gmail.com>
2015-03-01 16:36:52 +0800

    Site updated: 2015-03-01 16:36:52

---
+++ js/ripple.js
@@ -0,0 +1,30 @@
+var addRippleEffect = function (e) {
+    var target = e.target;
+    var flag = false;
+    var rippleLevel = target.getAttribute("ripple");
+    if (rippleLevel){
+        flag = true;
+        for (var i = 0; i < rippleLevel; i++) {
+            target = target.parentNode;
+        }
+    }
+    if(flag){
+        var rect = target.getBoundingClientRect();
+        var ripple = target.querySelector('.ripple');
+        if (!ripple) {
+            ripple = document.createElement('span');
+            ripple.className = 'ripple';
+            ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height) + 'px';
+            target.appendChild(ripple);
+        }
+        ripple.classList.remove('show');
+        var top = e.pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop;
+        var left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
+        ripple.style.top = top + 'px';
+        ripple.style.left = left + 'px';
+        ripple.classList.add('show');
+    }
+    return false;
+};
+
+document.addEventListener('click', addRippleEffect, false);
