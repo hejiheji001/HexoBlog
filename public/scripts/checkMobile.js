047cfe4a5fcbec8a23703a39e31f4bdd635fb40f
hejiheji001@gmail.com <hejiheji001@gmail.com>
2015-03-07 01:10:00 +0800

    Remove DS_Store

--- public/scripts/checkMobile.js
+++ public/scripts/checkMobile.js
@@ -1,16 +1,18 @@
 var checkMobile = function(id){
 	var md5 = "2e9cc3926668ac317e33bdd5548f3d98";
 	var mobi = document.getElementById(id).value;
-	var flag = false;
 	if(mobi){
 		if(typeof CryptoJS != "object"){
-			$.getScript('http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/md5.js',function(){
-				flag = md5 == CryptoJS.MD5(mobi).toString();
+			$.getScript('/scripts/md5.js',function(){
+				jump(id, md5 == CryptoJS.MD5(mobi).toString());
 			});
 		}else{
-			flag = md5 == CryptoJS.MD5(mobi).toString();
+			jump(id, md5 == CryptoJS.MD5(mobi).toString());
 		}
 	}
+}
+
+var jump = function(id, flag){
 	if(flag){
 		document.getElementById(id).style.cssText = "outline:rgb(72, 252, 33) auto 5px;outline-offset: -2px;";
 		setTimeout(function(){
